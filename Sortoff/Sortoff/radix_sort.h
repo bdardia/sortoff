b1ceed69a516cf03fddb1cca047d5fe539f71d8e
bdardia <31863287+bdardia@users.noreply.github.com>
2018-05-07 15:30:16 -0400

    Change parallel radix source

---
+++ Sortoff/Sortoff/radix_sort.h
@@ -0,0 +1,11 @@
+# pragma once
+typedef unsigned int uint;
+#include <amp.h>
+
+namespace pal
+{
+	void radix_sort(HDC hdc, uint* start, uint num, int fake = 0);
+	void radix_sort(HDC hdc, concurrency::array<uint>& arr);
+	int * radix_sort(HDC hdc, int(&arr)[726]);
+}
+
